472d1672f7359e0aedd98b2a38129e7c6c519eb9
Yamel Senih <yamelsenih@gmail.com>
2019-06-15 19:27:18 -0430

    Change license

--- base/org/spin/handler/FiscalPrinterElementHandler.java
+++ base/org/spin/handler/FiscalPrinterElementHandler.java
@@ -1,8 +1,8 @@
 /******************************************************************************
  * Product: Adempiere ERP & CRM Smart Business Solution                       *
- * Copyright (C) 1999-2006 Adempiere, Inc. All Rights Reserved.               *
  * This program is free software; you can redistribute it and/or modify it    *
- * under the terms version 2 of the GNU General Public License as published   *
+ * under the terms version 2 or later of the                                  *
+ * GNU General Public License as published                                    *
  * by the Free Software Foundation. This program is distributed in the hope   *
  * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
@@ -10,9 +10,10 @@
  * You should have received a copy of the GNU General Public License along    *
  * with this program; if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
- *                                                                            *
- * Copyright (C) 2005 Robert Klein. robeklein@hotmail.com                     *
- * Contributor(s): Low Heng Sin hengsin@avantz.com                            *
+ * For the text or an alternative of this public license, you may reach us    *
+ * Copyright (C) 2003-2015 E.R.P. Consultores y Asociados, C.A.               *
+ * All Rights Reserved.                                                       *
+ * Contributor(s): Yamel Senih www.erpya.com                                  *
  *****************************************************************************/
 package org.spin.handler;
 
