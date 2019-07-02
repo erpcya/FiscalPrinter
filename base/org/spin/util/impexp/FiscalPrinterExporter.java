02ff7e8039bb6ad9765b30459d777fbdcd2d67ae
Yamel Senih <ysenih@erpya.com>
2019-07-02 16:50:42 -0400

    Add UUID for fiscal printer

--- base/org/spin/util/impexp/FiscalPrinterExporter.java
+++ base/org/spin/util/impexp/FiscalPrinterExporter.java
@@ -32,9 +32,9 @@ import org.compiere.model.I_AD_Tab;
 import org.compiere.model.I_AD_Table;
 import org.compiere.model.I_AD_Window;
 import org.compiere.model.Query;
-import org.compiere.util.Env;
 import org.spin.model.I_AD_DeviceAttributeUse;
 import org.spin.model.I_AD_DeviceAttributeValue;
+import org.spin.model.I_AD_DeviceType;
 import org.spin.model.I_AD_DeviceTypeSetUse;
 import org.spin.model.I_AD_FP_Document;
 import org.spin.model.I_AD_FP_DocumentLine;
@@ -50,61 +50,66 @@ import org.xml.sax.SAXException;
 
 public class FiscalPrinterExporter extends GenericPOHandler {
 	public void create(Properties ctx, TransformerHandler document) throws SAXException {
-		int deviceTypeId = Env.getContextAsInt(ctx, "AD_DeviceType_ID");
 		PackOut packOut = (PackOut) ctx.get("PackOutProcess");
 		if(packOut == null ) {
 			packOut = new PackOut();
 			packOut.setLocalContext(ctx);
 		}
-		MADDeviceType deviceType = new MADDeviceType(ctx, deviceTypeId, null);
-		//	Exclude tables
-		List<String> tablesToExclude = new ArrayList<String>();
-		tablesToExclude.add(I_AD_Element.Table_Name);
-		tablesToExclude.add(I_AD_Column.Table_Name);
-		tablesToExclude.add(I_AD_Table.Table_Name);
-		tablesToExclude.add(I_AD_Window.Table_Name);
-		tablesToExclude.add(I_AD_Tab.Table_Name);
-		tablesToExclude.add(I_AD_Field.Table_Name);
-		//	Get Set Attributes
-		List<MADDeviceTypeSetUse> deviceTypeAttributeSetUseList = new Query(ctx, I_AD_DeviceTypeSetUse.Table_Name, I_AD_DeviceTypeSetUse.COLUMNNAME_AD_DeviceType_ID + " = ?", null)
-			.setParameters(deviceType.getAD_DeviceType_ID())
+		List<MADDeviceType> deviceTypeList = new Query(ctx, I_AD_DeviceType.Table_Name, 
+				"EXISTS(SELECT 1 FROM AD_FP_Document WHERE AD_DeviceType_ID = AD_DeviceType.AD_DeviceType_ID)", getTrxName(ctx))
+			.setOnlyActiveRecords(true)
+			.setClient_ID()
 			.list();
-		for(MADDeviceTypeSetUse deviceTypeAttributeSetUse : deviceTypeAttributeSetUseList) {
-			//	Attribute set
-			MADDeviceAttributeSet attributeSet = new MADDeviceAttributeSet(ctx, deviceTypeAttributeSetUse.getAD_DeviceAttributeSet_ID(), null);
-			packOut.createGenericPO(document, attributeSet, true, tablesToExclude);
-			//	Get List from Attribute Set
-			List<MADDeviceAttributeUse> deviceAttributeUseList = new Query(ctx, I_AD_DeviceAttributeUse.Table_Name, I_AD_DeviceAttributeUse.COLUMNNAME_AD_DeviceAttributeSet_ID + " = ?", null)
-					.setParameters(attributeSet.getAD_DeviceAttributeSet_ID())
-					.list();
-			for(MADDeviceAttributeUse deviceAttributeUse : deviceAttributeUseList) {
-				MADDeviceAttribute attribute = new MADDeviceAttribute(ctx, deviceAttributeUse.getAD_DeviceAttribute_ID(), null);
-				packOut.createGenericPO(document, attribute, true, tablesToExclude);
-				//	Get Values
-				List<MADDeviceAttributeValue> deviceAttributeValueList = new Query(ctx, I_AD_DeviceAttributeValue.Table_Name, I_AD_DeviceAttributeValue.COLUMNNAME_AD_DeviceAttribute_ID + " = ?", null)
-						.setParameters(attribute.getAD_DeviceAttribute_ID())
-						.list();
-				for(MADDeviceAttributeValue deviceAttributeValue : deviceAttributeValueList) {
-					packOut.createGenericPO(document, deviceAttributeValue, true, tablesToExclude);
-				}
-				packOut.createGenericPO(document, deviceAttributeUse, true, tablesToExclude);
-			}
-			//	
-			packOut.createGenericPO(document, deviceTypeAttributeSetUse, true, tablesToExclude);
-		}
-		//	For Fiscal Documents
-		List<MADFPDocument> fiscalDocumentList = new Query(ctx, I_AD_FP_Document.Table_Name, I_AD_FP_Document.COLUMNNAME_AD_DeviceType_ID + " = ?", null)
+		for(MADDeviceType deviceType : deviceTypeList) {
+			//	Exclude tables
+			List<String> tablesToExclude = new ArrayList<String>();
+			tablesToExclude.add(I_AD_Element.Table_Name);
+			tablesToExclude.add(I_AD_Column.Table_Name);
+			tablesToExclude.add(I_AD_Table.Table_Name);
+			tablesToExclude.add(I_AD_Window.Table_Name);
+			tablesToExclude.add(I_AD_Tab.Table_Name);
+			tablesToExclude.add(I_AD_Field.Table_Name);
+			//	Get Set Attributes
+			List<MADDeviceTypeSetUse> deviceTypeAttributeSetUseList = new Query(ctx, I_AD_DeviceTypeSetUse.Table_Name, I_AD_DeviceTypeSetUse.COLUMNNAME_AD_DeviceType_ID + " = ?", null)
 				.setParameters(deviceType.getAD_DeviceType_ID())
 				.list();
-		//	
-		for(MADFPDocument fiscalDocument : fiscalDocumentList) {
-			packOut.createGenericPO(document, fiscalDocument, true, tablesToExclude);
-			//	Get Lines
-			List<MADFPDocumentLine> fiscalDocumentLineList = new Query(ctx, I_AD_FP_DocumentLine.Table_Name, I_AD_FP_DocumentLine.COLUMNNAME_AD_FP_Document_ID + " = ?", null)
-					.setParameters(fiscalDocument.getAD_FP_Document_ID())
+			for(MADDeviceTypeSetUse deviceTypeAttributeSetUse : deviceTypeAttributeSetUseList) {
+				//	Attribute set
+				MADDeviceAttributeSet attributeSet = new MADDeviceAttributeSet(ctx, deviceTypeAttributeSetUse.getAD_DeviceAttributeSet_ID(), null);
+				packOut.createGenericPO(document, attributeSet, true, tablesToExclude);
+				//	Get List from Attribute Set
+				List<MADDeviceAttributeUse> deviceAttributeUseList = new Query(ctx, I_AD_DeviceAttributeUse.Table_Name, I_AD_DeviceAttributeUse.COLUMNNAME_AD_DeviceAttributeSet_ID + " = ?", null)
+						.setParameters(attributeSet.getAD_DeviceAttributeSet_ID())
+						.list();
+				for(MADDeviceAttributeUse deviceAttributeUse : deviceAttributeUseList) {
+					MADDeviceAttribute attribute = new MADDeviceAttribute(ctx, deviceAttributeUse.getAD_DeviceAttribute_ID(), null);
+					packOut.createGenericPO(document, attribute, true, tablesToExclude);
+					//	Get Values
+					List<MADDeviceAttributeValue> deviceAttributeValueList = new Query(ctx, I_AD_DeviceAttributeValue.Table_Name, I_AD_DeviceAttributeValue.COLUMNNAME_AD_DeviceAttribute_ID + " = ?", null)
+							.setParameters(attribute.getAD_DeviceAttribute_ID())
+							.list();
+					for(MADDeviceAttributeValue deviceAttributeValue : deviceAttributeValueList) {
+						packOut.createGenericPO(document, deviceAttributeValue, true, tablesToExclude);
+					}
+					packOut.createGenericPO(document, deviceAttributeUse, true, tablesToExclude);
+				}
+				//	
+				packOut.createGenericPO(document, deviceTypeAttributeSetUse, true, tablesToExclude);
+			}
+			//	For Fiscal Documents
+			List<MADFPDocument> fiscalDocumentList = new Query(ctx, I_AD_FP_Document.Table_Name, I_AD_FP_Document.COLUMNNAME_AD_DeviceType_ID + " = ?", null)
+					.setParameters(deviceType.getAD_DeviceType_ID())
 					.list();
-			for(MADFPDocumentLine fiscalDocumentLine : fiscalDocumentLineList) {
-				packOut.createGenericPO(document, fiscalDocumentLine, true, tablesToExclude);
+			//	
+			for(MADFPDocument fiscalDocument : fiscalDocumentList) {
+				packOut.createGenericPO(document, fiscalDocument, true, tablesToExclude);
+				//	Get Lines
+				List<MADFPDocumentLine> fiscalDocumentLineList = new Query(ctx, I_AD_FP_DocumentLine.Table_Name, I_AD_FP_DocumentLine.COLUMNNAME_AD_FP_Document_ID + " = ?", null)
+						.setParameters(fiscalDocument.getAD_FP_Document_ID())
+						.list();
+				for(MADFPDocumentLine fiscalDocumentLine : fiscalDocumentLineList) {
+					packOut.createGenericPO(document, fiscalDocumentLine, true, tablesToExclude);
+				}
 			}
 		}
 	}
